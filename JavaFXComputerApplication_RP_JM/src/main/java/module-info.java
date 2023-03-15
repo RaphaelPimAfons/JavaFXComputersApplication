b5365c3b02dd25b9318b4feb3cf6e49b2a6c0ed6
Raphael <raphael.pimenta@etml-es.ch>
2023-03-15 13:06:42 +0100

    Methode Computer et NetworkCard créé

--- JavaFXComputerApplication_RP_JM/src/main/java/module-info.java
+++ JavaFXComputerApplication_RP_JM/src/main/java/module-info.java
@@ -5,4 +5,6 @@ module com.example.javafxcomputerapplication_rp_jm {
 
     opens com.example.javafxcomputerapplication_rp_jm to javafx.fxml;
     exports com.example.javafxcomputerapplication_rp_jm;
+    exports com.example.javafxcomputermgnt;
+    opens com.example.javafxcomputermgnt to javafx.fxml;
 }
