d7c24da68cd33a35faca8493fb11e7b1a195048c
Josimar Monteiro <josimar.monteiro@etml-es.ch>
2023-03-22 11:26:26 +0100

    Taff du 22.03.23

--- JavaFXComputerApplication_RP_JM/src/main/java/com/example/javafxcomputerapplication_rp_jm/HelloController.java
+++ JavaFXComputerApplication_RP_JM/src/main/java/com/example/javafxcomputerapplication_rp_jm/HelloController.java
@@ -1,14 +1,88 @@
 package com.example.javafxcomputerapplication_rp_jm;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Button btnAjoutCarte;
+
+    @FXML
+    private Button btnEffacer;
+
+    @FXML
+    private Button btnEnregistrer;
+
+    @FXML
+    private Button btnExporter;
+
+    @FXML
+    private Button btnPing;
+
+    @FXML
+    private ComboBox<?> cmbAffCartes;
+
+    @FXML
+    private ComboBox<?> cmbListeRec;
+
+    @FXML
+    private Label lblAddIp;
+
+    @FXML
+    private Label lblModel;
+
+    @FXML
+    private Label lblNbProc;
+
+    @FXML
+    private Label lblNom;
+
+    @FXML
+    private Label lblQteMem;
+
+    @FXML
+    private Label lblQteStock;
+
+    @FXML
+    private Label lblSM;
+
+    @FXML
+    private ListView<?> lvListeView;
+
+    @FXML
+    private RadioButton rbtnLinux;
+
+    @FXML
+    private RadioButton rbtnMac;
+
+    @FXML
+    private RadioButton rbtnWindows;
+
+    @FXML
+    private TextField txtAddIp;
+
+    @FXML
+    private TextField txtModel;
+
+    @FXML
+    private TextField txtNbProc;
+
+    @FXML
+    private TextField txtNom;
+
+    @FXML
+    private TextField txtQteMem;
+
+    @FXML
+    private TextField txtQteStock;
+
+    @FXML
+    private TextField txtSM;
+
 }
